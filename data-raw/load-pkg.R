4d73233cb4c16deeb17189841012ec7ed5c1497f
huhuaping <huhuaping01@nwsuaf.edu.cn>
2022-10-17 18:37:40 +0800

    use data

---
+++ data-raw/load-pkg.R
@@ -0,0 +1,10 @@
+## code to prepare `load-pkg` dataset goes here
+
+require(tidyverse)
+require(magrittr)
+require(here)
+require(glue)
+require(httr)
+require(rvest)
+
+
--- data-raw/ruzone.R
+++ data-raw/ruzone.R
@@ -1,3 +1,5 @@
 ## code to prepare `ruzone` dataset goes here
 
+source("data-raw/load-pkg.R")
+
 usethis::use_data(ruzone, overwrite = TRUE)
